3522cc95c52927b5980070d16a0709ed3221e2a5
Sandesh Sharma <59257189+Sandesh-Sharma@users.noreply.github.com>
2020-10-01 23:32:39 +0545

    created useOfStructure.c
    C program to read and print employee's record using structure

---
+++ useOfStructure.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+/*structure declaration*/
+struct employee{
+    char    name[30];
+    int     empId;
+    float   salary;
+};
+ 
+int main()
+{
+    /*declare structure variable*/
+    struct employee emp;
+     
+    /*read employee details*/
+    printf("\nEnter details :\n");
+    printf("Name ?:");          gets(emp.name);
+    printf("ID ?:");            scanf("%d",&emp.empId);
+    printf("Salary ?:");        scanf("%f",&emp.salary);
+     
+    /*print employee details*/
+    printf("\nEntered detail is:");
+    printf("Name: %s"   ,emp.name);
+    printf("Id: %d"     ,emp.empId);
+    printf("Salary: %f\n",emp.salary);
+    return 0;
+}
